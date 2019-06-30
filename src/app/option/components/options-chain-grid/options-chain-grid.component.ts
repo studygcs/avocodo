04121136659bbeab845c8003f51ebd712dbd1dec
CS <study.gcs@gmail.com>
2019-06-30 20:06:04 +0530

    option chain

--- src/app/option/components/options-chain-grid/options-chain-grid.component.ts
+++ src/app/option/components/options-chain-grid/options-chain-grid.component.ts
@@ -11,19 +11,33 @@ export class OptionsChainGridComponent implements OnInit {
   constructor(private readonly optionsChainService: OptionsChainService) { }
 
   columnDefs = [
-    { headerName: 'Make', field: 'make' },
-    { headerName: 'Model', field: 'model' },
-    { headerName: 'Price', field: 'price' }
+    { headerName: 'OI', field: 'callOI' },
+    { headerName: 'C in OI', field: 'callChangeInOI' },
+    { headerName: 'Volume', field: 'callVolume' },
+    { headerName: 'IV', field: 'callIV' },
+    { headerName: 'LTP', field: 'callLTP' },
+    { headerName: 'Net C', field: 'callNetChng' },
+    { headerName: 'Bid Qty', field: 'callBidQty' },
+    { headerName: 'Bid Price', field: 'callBidPrice' },
+    { headerName: 'Ask Price', field: 'callAskPrice' },
+    { headerName: 'Ask Qty', field: 'callAskQty' },
+    { headerName: 'Strike \n Price', field: 'strikePrice' },
+    { headerName: 'Bid Qty', field: 'putBidQty' },
+    { headerName: 'Bid Price', field: 'putBidPrice' },
+    { headerName: 'Ask Price', field: 'putAskPrice' },
+    { headerName: 'Ask Qty', field: 'putAskQty' },
+    { headerName: 'Net C', field: 'putNetChng' },
+    { headerName: 'LTP', field: 'putLTP' },
+    { headerName: 'IV', field: 'putIV' },
+    { headerName: 'Volume', field: 'putVolume' },
+    { headerName: 'C in OI', field: 'putChangeInOI' },
+    { headerName: 'OI', field: 'putOI' },
   ];
 
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
+  rowData: any;
 
   ngOnInit() {
-    let optionchain = this.optionsChainService.getOptionChain();
+    this.rowData = this.optionsChainService.getOptionChain();
   }
 
 }
--- src/app/option/service/options-chain.service.ts
+++ src/app/option/service/options-chain.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import cheerio from 'cheerio';
 import cheerioTableparser from 'cheerio-tableparser';
 import { OptionCallPut } from "./../../lib/common-types";
+import { parse } from "querystring";
 
 
 let optionchain = 'https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&instrument=OPTIDX&symbol=NIFTY&date=-';
@@ -27,7 +28,7 @@ export class OptionsChainService {
                     // if (!error && response.statusCode == 200) 
                     {
                         console.log('in');
-                        const $ = cheerio.load(response);                       
+                        const $ = cheerio.load(response);
 
                         var table = $('div[class=opttbldata]').html();
 
@@ -35,10 +36,13 @@ export class OptionsChainService {
                         console.log(t);
 
                         cheerioTableparser(t);
-                        
+
                         var data = t("#octable").parsetable(true, true, true);
                         console.log(data);
-                        resolve(data);
+
+                        const arrOptionChain = this.getOptionsChain(data);
+                        console.log(arrOptionChain);
+                        resolve(arrOptionChain);
 
                         console.log('out');
 
@@ -55,12 +59,49 @@ export class OptionsChainService {
         return ocPromise;
     }
 
-    getOptionsChain(opChain) {
+    getOptionsChain(opChain): OptionCallPut[] {
 
-        let arrOptionChain: OptionCallPut[] = [];          
-        
+        let arrOptionChain: OptionCallPut[] = [];
+        for (let i = 2; i < opChain[0].length - 1; i++) {
+            let optionCallPut = new OptionCallPut();
+            optionCallPut.callOI = this.getNumber(opChain[1][i]);
+            optionCallPut.callChangeInOI = this.getNumber(opChain[2][i]);
+            optionCallPut.callVolume = this.getNumber(opChain[3][i]);
+            optionCallPut.callIV = this.getNumber(opChain[4][i]);
+            optionCallPut.callLTP = this.getNumber(opChain[5][i]);
+            optionCallPut.callNetChng = this.getNumber(opChain[6][i]);
+            optionCallPut.callBidQty = this.getNumber(opChain[7][i]);
+            optionCallPut.callBidPrice = this.getNumber(opChain[8][i]);
+            optionCallPut.callAskPrice = this.getNumber(opChain[9][i]);
+            optionCallPut.callAskQty = this.getNumber(opChain[10][i]);
 
+            optionCallPut.strikePrice = this.getNumber(opChain[11][i]);
+
+            optionCallPut.putOI = this.getNumber(opChain[21][i]);
+            optionCallPut.putChangeInOI = this.getNumber(opChain[20][i]);
+            optionCallPut.putVolume = this.getNumber(opChain[19][i]);
+            optionCallPut.putIV = this.getNumber(opChain[18][i]);
+            optionCallPut.putLTP = this.getNumber(opChain[17][i]);
+            optionCallPut.putNetChng = this.getNumber(opChain[16][i]);
+            optionCallPut.putBidQty = this.getNumber(opChain[15][i]);
+            optionCallPut.putBidPrice = this.getNumber(opChain[14][i]);
+            optionCallPut.putAskPrice = this.getNumber(opChain[13][i]);
+            optionCallPut.putAskQty = this.getNumber(opChain[12][i]);
+
+            arrOptionChain.push(optionCallPut);
+        }
+
+        return arrOptionChain;
     }
+
+    getNumber(num: string): number {
+        var parsed = parseFloat(num);
+        if (isNaN(parsed)) {
+            return 0;
+        }
+        return parsed;
+    }
+
 }
 
 
